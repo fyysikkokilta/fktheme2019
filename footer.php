14684882ae9f163bd417b5cf07fc400fa7ae7eb9
Alpi Rimppi <alpi@Alpis-MacBook-Pro.local>
2018-11-11 21:44:07 +0200

    Hide unnecessary footer information for now

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<div class="site-info">
 
-						<?php understrap_site_info(); ?>
+						<!-- <?php understrap_site_info(); ?> -->
 
 					</div><!-- .site-info -->
 
