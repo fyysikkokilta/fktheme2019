42798efd2896ea5fae75aaf942d90f14afe9a101
Alpi Rimppi <alpi.rimppi@aalto.fi>
2018-11-19 15:39:17 +0200

    Included FK custom functions partials

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $understrap_includes = array(
 	'/theme-settings.php',                  // Initialize theme default settings.
 	'/setup.php',                           // Theme setup and custom theme supports.
-	'/widgets.php',                         // Register widget area.
+	//'/widgets.php',                         // Register widget area.
 	'/enqueue.php',                         // Enqueue scripts and styles.
 	'/template-tags.php',                   // Custom template tags for this theme.
 	'/pagination.php',                      // Custom pagination for this theme.
@@ -27,6 +27,20 @@ $understrap_includes = array(
 );
 
 foreach ( $understrap_includes as $file ) {
+	$filepath = locate_template( '/inc' . $file );
+	if ( ! $filepath ) {
+		trigger_error( sprintf( 'Error locating /inc%s for inclusion', $file ), E_USER_ERROR );
+	}
+	require_once $filepath;
+}
+
+$fk_includes = array(						// Separating this from understrap only to make this more readable
+	'/fk-customizer.php', 					// Load own customizer content
+	'/fk-widgets.php',						// Load own widget preferences
+	'/fk-custom-post.php',					// Load own custom post types
+);
+
+foreach ( $fk_includes as $file ) {
 	$filepath = locate_template( '/inc' . $file );
 	if ( ! $filepath ) {
 		trigger_error( sprintf( 'Error locating /inc%s for inclusion', $file ), E_USER_ERROR );
