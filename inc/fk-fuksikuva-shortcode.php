84d94c2d70fa32a94c02da6a710dcdd01f72d81e
Alpi Rimppi <alpi.rimppi@aalto.fi>
2019-03-15 15:50:58 +0200

    Working implementation of fuksikuva-puller

--- inc/fk-fuksikuva-shortcode.php
+++ inc/fk-fuksikuva-shortcode.php
@@ -12,19 +12,42 @@
         $folder = $a['folder'];
         $title = $a['title'];
 
-        $folder_contents = glob(($dir . '/' . $folder . '/*') , GLOB_ONLYDIR);
-
+        $transient_name = $folder;
+        
         $out = '';
-        foreach ($folder_contents as $idx=>$fuksir) {
-            $files = array_diff(scandir($fuksir), array('..', '.'));
 
-            foreach($files as $fuksikuva) {
-                $out .= '<img src="' . $url . '/' . ' ' . '/' . $fuksikuva . '" class="">';
-                //$out .= $fuksikuva . ' ';
+        if( false === ($out = get_transient($transient_name))) {
+            
+
+            $folder_contents = glob(($dir . '/' . $folder . '/*') , GLOB_ONLYDIR);
+
+            $out = '';
+            $out .= '<div class="row"> <h2>' . $title . '</h3></div>'; 
+
+            foreach ($folder_contents as $idx=>$fuksir) {
+                $files = array_diff(scandir($fuksir), array('..', '.'));
+                $ryhma = end(explode("/", $fuksir));
+                
+                $out .= '<div class="row"> <h3>' . str_replace("_", " ", $ryhma) . '</h3></div>'; 
+
+                $out .= '<div class="row">';
+                foreach($files as $fuksikuva) {
+                    if(substr($fuksikuva, -3) === "jpg" || substr($fuksikuva, -4) === "jpeg" ) {
+                        $adress = $url . '/' . $folder  . '/' . $ryhma . '/' . $fuksikuva . ' ';
+                        $nimi = explode(".", $fuksikuva)[0];
+                        $nimi = str_replace("_", " ", $nimi);
+                        $out .= '<div class="col-md-2 col-sm-3 col-4">';
+                        $out .= '<img src="' . $adress . '" class="">';
+                        $out .= '<p>' . $nimi . '</p>';
+                        $out .= '</div>';
+                    }
+                }
+                $out .= '</div>';
             }
             
-        }
+            set_transient( $transient_name, $out, 60*60*24*30);
 
-        return implode($folder_contents);
+        }
+        return ($out);
 
     }
