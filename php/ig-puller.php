d0c15b3d31beb659743647ab46df7862ca843a96
Alpi Rimppi <alpi.rimppi@aalto.fi>
2019-03-22 16:46:42 +0200

    Debugging

--- php/ig-puller.php
+++ php/ig-puller.php
@@ -4,7 +4,6 @@
  */
 
 function fk_ig_getFeed($count) {
-    
 
     $cache_time = fk_get_theme_option( 'cache_timeout' );
     $transient_name = "fk_ig_data";
@@ -31,11 +30,13 @@ function fk_ig_getFeed($count) {
 function fk_ig_printEvents($count) {
     $feed = fk_ig_getFeed($count);
     //print_r($feed);
-
-    for($i = 0; $i < min(8, count($feed)); $i++) {
-        $ig_post = $feed[$i];
-        $thumb_url = $ig_post->images->standard_resolution->url;
-        $link = $ig_post->link;
-        echo '<div class="col-6 col-md-4 col-lg-3 ig-wrap"><div class="ig-item"><a href="'. $link .'" target="_blank"><img src="'. $thumb_url .'"></a></div></div>';
+    echo 'Doing stuff';
+    if($feed) {
+        for($i = 0; $i < min(8, count($feed)); $i++) {
+            $ig_post = $feed[$i];
+            $thumb_url = $ig_post->images->standard_resolution->url;
+            $link = $ig_post->link;
+            echo '<div class="col-6 col-md-4 col-lg-3 ig-wrap"><div class="ig-item"><a href="'. $link .'" target="_blank"><img src="'. $thumb_url .'"></a></div></div>';
+        }
     }
 }
