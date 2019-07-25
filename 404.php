f3beafc5cfcfc9285e01ea13c53637f77fbf6d2b
summis <summanentommi@gmail.com>
2019-07-25 20:46:01 +0300

    Revert "Remove stuff from 404 page"
    This reverts commit c0a0a7d10364c66d99c645b776ad01c9f757cf7a.

--- 404.php
+++ 404.php
@@ -28,15 +28,50 @@ $container   = get_theme_mod( 'understrap_container_type' );
 
 						<header class="page-header">
 
-							<h1 class="page-title"><?php esc_html_e( 'Huppista! Kaivattua sivua ei löydy :(',
+							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.',
 							'understrap' ); ?></h1>
 
-            </header><!-- .page-header -->
-            
+						</header><!-- .page-header -->
+
 						<div class="page-content">
 
-							<p><?php esc_html_e( 'Voit yrittää hakea etsimääsi tietoa:',
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?',
 							'understrap' ); ?></p>
+
+							<?php get_search_form(); ?>
+
+							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+
+							<?php if ( understrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+
+								<div class="widget widget_categories">
+
+									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'understrap' ); ?></h2>
+
+									<ul>
+										<?php
+										wp_list_categories( array(
+											'orderby'    => 'count',
+											'order'      => 'DESC',
+											'show_count' => 1,
+											'title_li'   => '',
+											'number'     => 10,
+										) );
+										?>
+									</ul>
+
+								</div><!-- .widget -->
+
+							<?php endif; ?>
+
+							<?php
+
+							/* translators: %1$s: smiley */
+							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'understrap' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+
+							the_widget( 'WP_Widget_Tag_Cloud' );
+							?>
 
 						</div><!-- .page-content -->
 
